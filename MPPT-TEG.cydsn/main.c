c7f6d39c77d635e05c43db6b39226dcacab54c42
Gavin Hurlbut <gjhurlbu@gmail.com>
2019-09-05 03:51:06 -0700

    Added DMA to SPI, filled out code.

--- MPPT-TEG.cydsn/main.c
+++ MPPT-TEG.cydsn/main.c
@@ -13,26 +13,54 @@
 #include "tegchannel.h"
 #include "tca9534.h"
 
+teg_channel_t transfer_buffer[TEG_CHANNEL_COUNT];
+static volatile _Bool dma_complete_flag;
+
+void dma_complete(void) {
+    uint32 mask = CyDmaGetInterruptSource();
+    dma_complete_flag = 1;
+    CyDmaClearInterruptSource(mask);
+}
+
 int main(void)
 {
     int i;
     uint32 lastTimer;
     uint8 newEnables;
     
-    // Disable Timer 0
-    // Set it up for 10Hz, cascading to Timer 1
+    // Disable all WDT Timers
+    CySysWdtUnlock();
+    CySysWdtDisable(CY_SYS_WDT_COUNTER0_MASK | CY_SYS_WDT_COUNTER1_MASK | CY_SYS_WDT_COUNTER2_MASK);
+    
+    // Setup WDT0 as a WDT, set WDT1 as 10Hz, cascading to WDT2
+    CySysWdtSetMode(0, CY_SYS_WDT_MODE_INT_RESET);
+    CySysWdtSetMode(1, CY_SYS_WDT_MODE_NONE);
+    CySysWdtSetMode(2, CY_SYS_WDT_MODE_NONE);
+    CySysWdtSetClearOnMatch(1, 1);
+    CySysWdtSetMatch(1, 3999);  // 40kHz source -> 10Hz cascade into timer 2
+    CySysWdtResetCounters(CY_SYS_WDT_COUNTER0_MASK | CY_SYS_WDT_COUNTER1_MASK | CY_SYS_WDT_COUNTER2_MASK);
+    CySysWdtSetCascade(CY_SYS_WDT_CASCADE_12);
+    
     // Enable Timer 0 and 1
+    CySysWdtEnable(CY_SYS_WDT_COUNTER0_MASK | CY_SYS_WDT_COUNTER1_MASK | CY_SYS_WDT_COUNTER2_MASK);
+    CySysWdtLock();
     
     lastTimer = 0;
     
     initializeChannels();
     
+    DMA_1_Init();
+    DMA_1_SetInterruptCallback(dma_complete);
+    dma_complete_flag = 0;
+    CyDmaEnable();
+    
     CyGlobalIntEnable; /* Enable global interrupts. */
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     
     for(;;)
     {
+        // Feed the watchdog to keep the system from resetting
+        CySysWatchdogFeed(CY_SYS_WDT_COUNTER0);
+        
         // Read the enable DIP switch via I2C 0x38
         newEnables = TCA9534_read(TCA9534_ENABLES_INPUT);
         
@@ -47,12 +75,28 @@ int main(void)
         }
 
         // Now we want to send the entire teg_channel block to the CPU for display and upstream to IOT
+        memcpy(transfer_buffer, teg_channels, sizeof(transfer_buffer));
+        SPI_1_SpiSetActiveSlaveSelect(SPI_1_SPI_SLAVE_SELECT0);
+        SPI_1_Start();
+        
+        dma_complete_flag = 0;
+        DMA_1_SetDstAddress(0, (void *)SPI_1_TX_FIFO_WR_PTR);
+        DMA_1_SetSrcAddress(0, transfer_buffer);
+        DMA_1_SetNumDataElements(0, sizeof(transfer_buffer));
+        DMA_1_ValidateDescriptor(0);
+        DMA_1_ChEnable();
+        
+        while(!dma_complete_flag) {
+            CySysPmSleep();
+        }
+        
+        SPI_1_Stop();
         
         // And finally, wait for the 100ms timer to tick, and do it all again
-        //while(lastTimer == CySysTimerGetCount(1)) {
-        //   CySysPmSleep();
-        //}
-        //lastTimer = CySysTimerGetCount(1);
+        while(lastTimer == CySysWdtGetCount(1)) {
+           CySysPmSleep();
+        }
+        lastTimer = CySysWdtGetCount(1);
     }
 }
 
